1784f6cd27679b8095ea96853079d873f02db6d4
yeidan79 <1181241@isep.ipp.pt>
2020-07-27 20:42:26 +0100

    Changing the PORT and message when route

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("It works!") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
