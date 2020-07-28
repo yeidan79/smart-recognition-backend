2ea2c1ba548bdb6ba23286885890bd7c56c03aef
yeidan79 <1181241@isep.ipp.pt>
2020-07-28 18:42:28 +0100

    Error in register

--- controllers/register.js
+++ controllers/register.js
@@ -30,7 +30,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => console.log(err);)
 }
 
 module.exports = {
